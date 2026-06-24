f977d5298849767b2174e8cca6423a30ae5a7a62
spiefer87-del <spiefer87@googlemail.com>
2026-06-24 22:54:49 +0200

    Add get_profile function to manage user profile

--- core/profile.py
+++ core/profile.py
@@ -1,1 +1,18 @@
+def get_profile():
 
+    now_min = minutes_now()
+
+    day_start = int(config["DAY_START_MIN"])
+    night_start = int(config["NIGHT_START_MIN"])
+
+    if is_night(now_min, night_start, day_start):
+        profile = "NACHT"
+    else:
+        profile = "TAG"
+
+    if profile != state.current_profile:
+        state.current_profile = profile
+
+    state.live_state["profile"] = profile
+
+    return profile
