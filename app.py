82ccf37a0ff689fa7d5469101a1e495f9876be5c
spiefer87-del <spiefer87@googlemail.com>
2026-06-25 11:50:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import presets
 from collections import deque
 
 import core.state as state
-from core.config import config, safe_config
+from core.config import config, save_config
 
 from core.helpers import (
         calculate_vpd,
