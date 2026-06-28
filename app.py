50aae0fcd11835f1d44311ed0910bc7495d73b62
spiefer87-del <spiefer87@googlemail.com>
2026-06-28 17:50:44 +0200

    Simplify state routes registration in app.py

--- app.py
+++ app.py
@@ -712,9 +712,7 @@ def watchdog_loop():
 flask_app = Flask(__name__)
 
 register_dashboard_routes(flask_app)
-register_state_routes(
-    flask_app,
-)
+register_state_routes(flask_app)
 register_plants_routes(flask_app)
 register_diary_routes(flask_app)
 register_diagrams_routes(flask_app)
