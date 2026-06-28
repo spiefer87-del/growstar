d8f178952a75c4b3f07a91964531edac570b39f2
spiefer87-del <spiefer87@googlemail.com>
2026-06-28 20:59:50 +0200

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1,1 +1,13 @@
 SENSOR_TIMEOUT = 120
+
+ENERGY_DEVICES = {
+    "heating": ("IP_HEATING", "RELAY_HEATING"),
+    "light": ("IP_LIGHT", "RELAY_LIGHT"),
+    "fan": ("IP_FAN", "RELAY_FAN"),
+    "vent": ("IP_VENT", "RELAY_VENT"),
+    "irrigation": ("IP_IRRIGATION", "RELAY_IRRIGATION"),
+    "humidifier": ("IP_HUMIDIFIER", "RELAY_HUMIDIFIER"),
+    "dehumidifier": ("IP_DEHUMIDIFIER", "RELAY_DEHUMIDIFIER"),
+    "light2": ("IP_LIGHT2", "RELAY_LIGHT2"),
+    "vent2": ("IP_VENT2", "RELAY_VENT2"),
+}
