ad9c4fe0b7e7fb5545325ea3435a4f678a4f56a9
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:40:55 +0200

    Add HardwareScanner class with scan_gateways method

--- core/hardware/scanner.py
+++ core/hardware/scanner.py
@@ -1,1 +1,13 @@
+from .registry import registry
 
+
+class HardwareScanner:
+
+    def scan_gateways(self):
+
+        for scanner in registry.gateway_scanners_list():
+
+            scanner.scan()
+
+
+scanner = HardwareScanner()
