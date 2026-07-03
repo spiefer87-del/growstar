179e6f68634143fbeaf8dd07dbfc7ecb278cf415
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:30:18 +0200

    Add Actuator class extending HardwareDevice

--- core/hardware/actuator.py
+++ core/hardware/actuator.py
@@ -1,1 +1,12 @@
+from .device import HardwareDevice
 
+from .types import ACTUATOR
+
+
+class Actuator(HardwareDevice):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.type = ACTUATOR
