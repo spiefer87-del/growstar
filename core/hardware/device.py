f71376f271c106be374f77561153f4dece84b105
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:29:44 +0200

    Add HardwareDevice dataclass with attributes

--- core/hardware/device.py
+++ core/hardware/device.py
@@ -1,1 +1,19 @@
+from dataclasses import dataclass, field
 
+
+@dataclass
+class HardwareDevice:
+
+    id: str = ""
+
+    name: str = ""
+
+    manufacturer: str = ""
+
+    model: str = ""
+
+    type: str = ""
+
+    online: bool = False
+
+    properties: dict = field(default_factory=dict)
