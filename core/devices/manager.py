1bf3258ef8104d44db734e912ef6880c3c635a80
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 22:24:36 +0200

    Add DeviceManager class for device management
    Implemented DeviceManager class to manage devices.

--- core/devices/manager.py
+++ core/devices/manager.py
@@ -1,1 +1,33 @@
+from typing import Dict
 
+from .models import Device
+
+
+class DeviceManager:
+
+    def __init__(self):
+
+        self.devices: Dict[str, Device] = {}
+
+    def add(self, device: Device):
+
+        self.devices[device.id] = device
+
+    def remove(self, device_id):
+
+        self.devices.pop(device_id, None)
+
+    def get(self, device_id):
+
+        return self.devices.get(device_id)
+
+    def all(self):
+
+        return list(self.devices.values())
+
+    def clear(self):
+
+        self.devices.clear()
+
+
+manager = DeviceManager()
