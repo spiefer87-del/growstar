c0a35e7c06a45613ed6b3bb1c2afd7a793797479
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 22:25:23 +0200

    Refactor ShellyGateway to improve device info handling

--- core/devices/shelly/gateway.py
+++ core/devices/shelly/gateway.py
@@ -1,43 +1,52 @@
+from core.devices.models import Device
+
 from .api import ShellyAPI
 
 
-class ShellyGateway:
+class ShellyGateway(Device):
 
     def __init__(self, ip):
 
+        super().__init__()
+
         self.ip = ip
+
+        self.manufacturer = "Shelly"
+
         self.api = ShellyAPI(ip)
-
-        self.online = False
-
-        self.name = ""
-        self.model = ""
-        self.mac = ""
-        self.firmware = ""
-
-        self.bluetooth = False
 
     def refresh(self):
 
-        info = self.api.get_device_info()
+        info = self.api.device_info()
 
         if not info:
+
             self.online = False
+
             return False
 
         self.online = True
 
+        self.id = info.get("mac", self.ip)
+
         self.name = info.get("name", "")
+
         self.model = info.get("model", "")
+
         self.mac = info.get("mac", "")
+
         self.firmware = info.get("fw_id", "")
 
-        config = self.api.get_config()
+        config = self.api.config()
 
         if config:
 
-            ble = config.get("ble", {})
-
-            self.bluetooth = ble.get("enable", False)
+            self.properties["bluetooth"] = config.get(
+                "ble",
+                {}
+            ).get(
+                "enable",
+                False
+            )
 
         return True
