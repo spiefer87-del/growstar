55acba36293ab58f6862a432ced54e58fbec4270
spiefer87-del <spiefer87@googlemail.com>
2026-07-04 00:53:29 +0200

    Translate mDNS discovery messages to German
    Updated print statements to use German language for mDNS discovery messages.

--- core/hardware/shelly/mdns.py
+++ core/hardware/shelly/mdns.py
@@ -1,11 +1,61 @@
+import socket
+import time
+
+from zeroconf import Zeroconf, ServiceBrowser, ServiceListener
+
+
+class ShellyListener(ServiceListener):
+
+    def __init__(self):
+
+        self.devices = []
+
+    def add_service(self, zc, service_type, name):
+
+        info = zc.get_service_info(service_type, name)
+
+        if not info:
+            return
+
+        addresses = info.parsed_addresses()
+
+        if not addresses:
+            return
+
+        ip = addresses[0]
+
+        print(f"Shelly gefunden: {name} ({ip})")
+
+        self.devices.append({
+            "name": name,
+            "ip": ip
+        })
+
+    def update_service(self, zc, service_type, name):
+        pass
+
+    def remove_service(self, zc, service_type, name):
+        pass
+
+
 class ShellyMDNS:
 
     def scan(self):
 
-        print("mDNS Discovery")
+        print("mDNS Suche...")
 
-        #
-        # folgt später
-        #
+        zc = Zeroconf()
 
-        return []
+        listener = ShellyListener()
+
+        ServiceBrowser(
+            zc,
+            "_http._tcp.local.",
+            listener
+        )
+
+        time.sleep(3)
+
+        zc.close()
+
+        return listener.devices
