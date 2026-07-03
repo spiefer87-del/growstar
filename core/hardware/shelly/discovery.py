7d03d62ba81ff1a584e1ebdd590255e348e50201
spiefer87-del <spiefer87@googlemail.com>
2026-07-04 00:33:52 +0200

    Refactor ShellyDiscovery scan method for clarity

--- core/hardware/shelly/discovery.py
+++ core/hardware/shelly/discovery.py
@@ -2,4 +2,30 @@ class ShellyDiscovery:
 
     def scan(self):
 
-        print(">>> Shelly Discovery läuft")
+        print(">>> Shelly Discovery")
+
+        gateways = []
+
+        gateways.extend(
+            self.scan_mdns()
+        )
+
+        if not gateways:
+
+            gateways.extend(
+                self.scan_network()
+            )
+
+        return gateways
+
+    def scan_mdns(self):
+
+        print("  -> mDNS Suche")
+
+        return []
+
+    def scan_network(self):
+
+        print("  -> Netzwerk Scan")
+
+        return []
