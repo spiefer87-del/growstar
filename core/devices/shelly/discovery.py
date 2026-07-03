b4e0136ff31042025c2e68109bb7ceed2fae606d
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 22:25:57 +0200

    Update ShellyDiscovery to integrate with manager
    Refactor ShellyDiscovery to use manager for device handling.

--- core/devices/shelly/discovery.py
+++ core/devices/shelly/discovery.py
@@ -1,11 +1,9 @@
+from core.devices.manager import manager
+
 from .gateway import ShellyGateway
 
 
 class ShellyDiscovery:
-
-    def __init__(self):
-
-        self.devices = {}
 
     def add(self, ip):
 
@@ -13,12 +11,8 @@ class ShellyDiscovery:
 
         if gateway.refresh():
 
-            self.devices[ip] = gateway
+            manager.add(gateway)
 
             return gateway
 
         return None
-
-    def all(self):
-
-        return list(self.devices.values())
