f3c0209f6600342098837494fa1a756eedd49cb3
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:30:01 +0200

    Add Gateway class for hardware management

--- core/hardware/gateway.py
+++ core/hardware/gateway.py
@@ -1,1 +1,20 @@
+from .device import HardwareDevice
 
+from .types import GATEWAY
+
+
+class Gateway(HardwareDevice):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.type = GATEWAY
+
+        self.ip = ""
+
+        self.mac = ""
+
+        self.firmware = ""
+
+        self.bluetooth = False
