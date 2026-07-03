3ee3ab0a79ad4b0a99ef49c8b3b773cb2058afdb
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:40:36 +0200

    Add HardwareRegistry class for managing scanners

--- core/hardware/registry.py
+++ core/hardware/registry.py
@@ -1,1 +1,16 @@
+class HardwareRegistry:
 
+    def __init__(self):
+
+        self.gateway_scanners = []
+
+    def register_gateway_scanner(self, scanner):
+
+        self.gateway_scanners.append(scanner)
+
+    def gateway_scanners_list(self):
+
+        return self.gateway_scanners
+
+
+registry = HardwareRegistry()
