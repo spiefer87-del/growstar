44621b9575aa837bb2bb2484e414bb7f3db6f0b8
spiefer87-del <spiefer87@googlemail.com>
2026-07-03 23:56:06 +0200

    Enhance scan_gateways with debug prints
    Add print statements to display HardwareScanner information and list of gateway scanners.

--- core/hardware/scanner.py
+++ core/hardware/scanner.py
@@ -5,6 +5,10 @@ class HardwareScanner:
 
     def scan_gateways(self):
 
+        print("HardwareScanner")
+        print(registry.gateway_scanners_list())
+
+
         for scanner in registry.gateway_scanners_list():
 
             scanner.scan()
