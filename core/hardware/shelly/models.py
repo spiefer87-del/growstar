5d94daddfba6299b963cb40e20e759a5f0466e1c
spiefer87-del <spiefer87@googlemail.com>
2026-07-04 12:22:16 +0200

    Add model names for Shelly devices

---
+++ core/hardware/shelly/models.py
@@ -0,0 +1,12 @@
+MODEL_NAMES = {
+
+    # Gen2
+    "SNPL-00112EU": "Shelly Plus Plug S",
+
+    # Gen3
+    "S3PL-30110EU": "Shelly Plug Mini Gen3",
+
+    # Power Strip Gen4
+    "S4PL-00416EU": "Shelly Power Strip",
+
+}
