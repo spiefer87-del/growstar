8a697410dfa2bd596ebaeb42e111bf3564df2cb9
spiefer87-del <spiefer87@googlemail.com>
2026-07-04 13:48:30 +0200

    Add hardware loop for periodic gateway refresh
    Implement a hardware loop to refresh gateways every 30 seconds.

--- threads/hardware.py
+++ threads/hardware.py
@@ -1,1 +1,20 @@
+import time
 
+from core.hardware.manager import manager
+
+
+def hardware_loop():
+
+    while True:
+
+        for gateway in manager.gateways_list():
+
+            try:
+
+                gateway.refresh()
+
+            except Exception as e:
+
+                print(e)
+
+        time.sleep(30)
