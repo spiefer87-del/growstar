a92e905f1b4d0d248a4656be8ccfeddf00bd82ea
spiefer87-del <spiefer87@googlemail.com>
2026-07-05 22:20:27 +0200

    Enhance RPC call logging in api.py
    Added logging for RPC method calls and responses.

--- core/hardware/shelly/api.py
+++ core/hardware/shelly/api.py
@@ -10,21 +10,25 @@ class ShellyAPI:
     def call(self, method, params=None):
 
         try:
-
+    
             response = requests.post(
                 f"{self.base}/{method}",
                 json=params or {},
                 timeout=5
             )
-
+    
+            print("RPC:", method)
+            print("Status:", response.status_code)
+            print("Antwort:", response.text)
+    
             response.raise_for_status()
-
+    
             return response.json()
-
+    
         except Exception as e:
-
+    
             print("RPC Fehler:", e)
-
+    
             return None
 
 
