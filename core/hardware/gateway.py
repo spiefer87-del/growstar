8157776ccc7671a62a1924597491ae539209205f
spiefer87-del <spiefer87@googlemail.com>
2026-07-05 22:52:34 +0200

    Refactor gateway.py for Bluetooth scanning support

--- core/hardware/gateway.py
+++ core/hardware/gateway.py
@@ -19,8 +19,10 @@ class Gateway(HardwareDevice):
         self.bluetooth = False
 
         self.bluetooth_enabled = False
-        
+
         self.bluetooth_scanning = False
+
+        self.bluetooth_scan = {}
 
         self.rssi = None
 
@@ -34,36 +36,40 @@ class Gateway(HardwareDevice):
     def to_dict(self):
 
         data = super().to_dict()
-    
+
         data.update({
-    
+
             "ip": self.ip,
-    
+
             "mac": self.mac,
-    
+
             "firmware": self.firmware,
-    
+
             "bluetooth": self.bluetooth,
-    
+
             "bluetooth_enabled": self.bluetooth_enabled,
-    
+
             "bluetooth_scanning": self.bluetooth_scanning,
 
             "bluetooth_scan": self.bluetooth_scan,
-    
+
             "rssi": self.rssi,
-    
+
             "uptime": self.uptime,
 
             "methods": self.methods,
 
             "capabilities": self.capabilities
 
-    
         })
-    
+
         return data
 
+
     def supports(self, method):
+
+        if not self.methods:
+
+            return False
 
         return method in self.methods
