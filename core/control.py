14e140418b8b945cbdb59f63084567cc267df869
spiefer87-del <spiefer87@googlemail.com>
2026-07-11 16:54:37 +0200

    Remove apply_sensor_assignments call from control_device
    Removed the call to apply_sensor_assignments() in control_device function.

--- core/control.py
+++ core/control.py
@@ -8,7 +8,6 @@ from core.config import config
 from core.profile import get_profile
 from core.ramp import get_ramped_target
 from core.helpers import minutes_now, in_time_window
-from core.sensors import apply_sensor_assignments
 from core.actuators import (
     set_device,
     set_heating,
@@ -116,8 +115,6 @@ def evaluate_env_conditions(device):
 
 
 def control_device(device):
-
-    apply_sensor_assignments()
 
     mode = get_device_mode(device)
     params = get_device_params(device)
